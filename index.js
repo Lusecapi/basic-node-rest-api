f49173e1c2376ea6a1608b523b2289f33a48748b
Lusecapi <lusecapi@hotmail.com>
2020-11-26 10:58:20 -0500

    Basic CORS handling. basic security helmet

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const helmet = require("helmet");
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+});
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
